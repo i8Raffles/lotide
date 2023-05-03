c97859d4f49201fc37256486cb8aecb8ee0da38a
Md Muhtasim Yasser <muhtasimyasser@gmail.com>
2023-05-03 18:09:53 -0400

    added object equal assertion function

---
+++ assertObjectsEqual.js
@@ -0,0 +1,39 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const eqObjects = function(object1, object2) {
+  let object1Array = Object.keys(object1);
+  let object2Array = Object.keys(object2);
+  if (object1Array.length !== object2Array.length) {
+    return false;
+  } else {
+    for (let key in object1) {
+      if ((Array.isArray(object1[key]) && Array.isArray(object2[key]))) {
+        if (eqArrays(object1[key], object2[key]) === false) {
+          return false;
+        }
+      } else {
+        if (object1[key] !== object2[key]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+assertObjectsEqual({ a: "1", b: 2 }, { b: 2, a: "1" });
