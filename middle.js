ef6dec942e2708a3518253381beefa0702147f45
Md Muhtasim Yasser <muhtasimyasser@gmail.com>
2023-05-03 13:42:44 -0400

    Added function to check middle of array

---
+++ middle.js
@@ -0,0 +1,23 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i])
+      return false;
+  }
+  return true;
+};
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+const middle = function(array) {
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    return [array[array.length / 2 - 1], array[array.length / 2]];
+  } else if (array.length % 2 !== 0) {
+    return array[Math.floor(array.length / 2)];
+  }
+};
