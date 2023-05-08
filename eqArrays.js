1077959dbbdeec1e6211fa63e3f0c68f4199de4d
Md Muhtasim Yasser <muhtasimyasser@gmail.com>
2023-05-08 13:53:42 -0400

    added eqArrays test

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
@@ -14,4 +6,5 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+module.exports = eqArrays;
