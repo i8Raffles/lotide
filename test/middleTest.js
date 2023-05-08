69978ec219ace5cca6936fc4f3e7dc3ce7cf5665
Md Muhtasim Yasser <muhtasimyasser@gmail.com>
2023-05-08 14:54:50 -0400

    refactored middleTest with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,21 @@
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
-middle([1]); // => []
-middle([1, 2]) // => []
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-middle([1, 2, 3, 4]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [[1, 2, 3, 4, 5, 6]]); //Assertion failed: [3, 4] !== 1,2,3,4,5,6
+
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [] for [1,2]", () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+  it("returns [2] for [1,2,3]", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+  it("returns [3] for [1,2,3,4,5]", () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+  it("returns [2,3] for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+});
